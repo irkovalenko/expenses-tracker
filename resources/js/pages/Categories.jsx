67b4db90af7c1f472731d487657b7ed10d4ce486
Irina Kovalenko <105125582+irkovalenko@users.noreply.github.com>
2026-06-19 18:02:08 +0200

    sidebar (#2)

--- resources/js/pages/Categories.jsx
+++ resources/js/pages/Categories.jsx
@@ -12,7 +12,6 @@ export default function Categories() {
         >
             <Head title="Categories" />
 
-            <div className="py-12">
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
@@ -20,7 +19,6 @@ export default function Categories() {
                         </div>
                     </div>
                 </div>
-            </div>
         </AuthenticatedLayout>
     );
 }
